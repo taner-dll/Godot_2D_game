cdec58cb620dc4ecaadb5656f6c239cbc4cc0595
Taner Deliloglu <tnrdll@gmail.com>
2023-09-13 04:08:07 +0300

    main game scene

--- Mob.gd
+++ Mob.gd
@@ -1,15 +1,16 @@
 extends RigidBody2D
 
 
-# Called when the node enters the scene tree for the first time.
+# animasyonu oynatıyoruz ve üç animasyon türünden birini rastgele seçiyoruz:
 func _ready():
+	
+	# mob_types is an array ["walk", "swim", "fly"]
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+	
 
-
-# Mobların ekrandan çıktıklarında kendilerini silmelerini sağlamaktır. 
+# Son parça ise çetelerin ekrandan çıktıklarında kendilerini silmelerini sağlamaktır. 
 # VisibleOnScreenNotifier2D düğümünün screen_exited() sinyalini Mob'a bağlayın
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free() # bellekten sil
-	
+	queue_free()
 	
